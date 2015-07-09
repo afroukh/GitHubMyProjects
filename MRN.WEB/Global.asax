1b33502693307dadf4980e928057012a44680357
afroukh <Florida123>
2015-07-09 12:02:16 +0000

    FirstCommit

---
+++ MRN.WEB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MRN.WEB.MvcApplication" Language="C#" %>
---
+++ MRN.WEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MRN.WEB.Startup))]
+namespace MRN.WEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
